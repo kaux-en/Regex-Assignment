874825df48c9293ff3cf3b3f827ad9fa75cf8ef1
keeks-co <08araik@gmail.com>
2024-10-10 19:04:17 -0500

    Regex Assignment

---
+++ #Python Regular Expressions Deep Dive.py
@@ -0,0 +1,12 @@
+#Python Regular Expressions Deep Dive
+
+#Task 1: Email Extraction Enhancement
+
+import re
+
+text = "Emails: user1@domain.com, user2@exclude.com, user3@domain.com"
+emails = re.findall(r"\b[A-Za-z0-9._%+-]+@[domain.-]+\.[A-Z|a-z]{2,}\b", text)
+print(emails)
+
+
+
